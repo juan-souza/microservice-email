e1aeb133a4960b5354792c582cfd2876ab5c9439
Juan Souza <juanccdesouza@gmail.com>
2022-03-08 19:39:22 -0300

    Initial commit

---
+++ src/test/java/com/jccs/ms/email/MsEmailApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jccs.ms.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
